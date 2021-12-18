5f251204d4aa83a24974015db839ec490a442448
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2021-12-19 02:11:59 +0530

    feat: rainbow Mode

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.querySelector('#draw');
 const strokeSize = document.querySelector('.stroke-size-input');
 const strokeColor = document.querySelector('.stroke-color-input');
+const rainbowBtn = document.querySelector('.rainbow');
+rainbowBtn.addEventListener('click', rainbowClickHandler);
 
 const ctx = canvas.getContext('2d');
 // canvas.width = window.innerWidth;
@@ -12,15 +14,36 @@ let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 ctx.lineWidth = 10;
+let hue = 0;
+let rainBow = false;
+function rainbowClickHandler(){
+    if(!rainBow){
+        rainBow = true;
+    }else{
+        rainBow = false;
+    }
+    
+    // ctx.strokeStyle = `hsl(${hue}, 100% , 50%)`;
+    // hue++;
+    // console.log(hue);
+}
 function draw(e){
     if(!isDrawing) return;
     // console.log(e);
+    if(rainBow){
+    ctx.strokeStyle = `hsl(${hue}, 100% , 50%)`;
+    hue++;
+    }else{
+        ctx.strokeStyle = 'black';
+    }
+    
     ctx.beginPath();
     ctx.moveTo(lastX, lastY);
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
     lastX = e.offsetX;
     lastY = e.offsetY;
+    
 }
 canvas.addEventListener('mousemove',draw);
 canvas.addEventListener('mousedown',(e)=> {
